5a9360891a44bcc141ebeba08b2d5d20696d5502
chertik77 <babychdenya@gmail.com>
2023-10-14 14:05:44 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,17 @@ import { Component } from 'react';
 import { Form } from '~~/Form/Form';
 import { Filter } from '~~/Filter/Filter';
 import { Contacts } from '~~/Contacts/Contacts';
+import users from '~/Users/Users';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: users,
     filter: '',
   };
 
   componentDidMount() {
     const savedContacts = localStorage.getItem('contacts');
-    if (!savedContacts) {
+    if (savedContacts !== null) {
       this.setState({ contacts: JSON.parse(savedContacts) });
     }
   }
