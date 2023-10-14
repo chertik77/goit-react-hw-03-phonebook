47614719f8d052fd395be44eea2c58ad08e841cf
chertik77 <babychdenya@gmail.com>
2023-10-14 12:22:57 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,15 @@ import { Component } from 'react';
 import { Form } from '~~/Form/Form';
 import { Filter } from '~~/Filter/Filter';
 import { Contacts } from '~~/Contacts/Contacts';
-import users from '../Users/users.json';
 
 export class App extends Component {
   state = {
-    contacts: users,
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
