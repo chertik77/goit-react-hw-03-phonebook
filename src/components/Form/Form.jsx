595fe11fa4e18137605163cb19475c0cab9a6edb
chertik77 <babychdenya@gmail.com>
2023-11-01 10:01:57 +0000

    updates

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,31 +1,31 @@
-import { nanoid } from 'nanoid';
-import { useState } from 'react';
-import { showConfirmMessage } from '~/Notifications/Notifications';
-import { isUserExistsByName, isUserExistsByNumber } from '../../helpers/IsUserExists';
+import { nanoid } from 'nanoid'
+import { useState } from 'react'
+import { showConfirmMessage } from '../../Notifications/Notifications'
+import { isUserExistsByName, isUserExistsByNumber } from '../../helpers/IsUserExists'
 
 export const Form = ({ contacts, newUser }) => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-  const hasSameUserName = isUserExistsByName(contacts, name);
-  const hasSameUserNumber = isUserExistsByNumber(contacts, number);
-  const hasSameUser = hasSameUserName && hasSameUserNumber;
-  const id = nanoid();
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+  const hasSameUserName = isUserExistsByName(contacts, name)
+  const hasSameUserNumber = isUserExistsByNumber(contacts, number)
+  const hasSameUser = hasSameUserName && hasSameUserNumber
+  const id = nanoid()
 
   const onFormSubmit = e => {
-    e.preventDefault();
+    e.preventDefault()
 
     if (hasSameUser || hasSameUserName || hasSameUserNumber) {
-      showConfirmMessage().then(() => newUser({ id, name, number }));
+      showConfirmMessage().then(() => newUser({ id, name, number }))
     } else {
-      newUser({ id, name, number });
+      newUser({ id, name, number })
     }
-    resetForm();
-  };
+    resetForm()
+  }
 
   const resetForm = () => {
-    setName('');
-    setNumber('');
-  };
+    setName('')
+    setNumber('')
+  }
 
   return (
     <form onSubmit={onFormSubmit}>
@@ -49,10 +49,9 @@ export const Form = ({ contacts, newUser }) => {
       />
       <button
         type='submit'
-        className='mx-auto block rounded-md bg-orange-400 px-5 py-2 font-serif text-white'
-      >
+        className='mx-auto block rounded-md bg-orange-400 px-5 py-2 font-serif text-white'>
         Add contact
       </button>
     </form>
-  );
-};
+  )
+}
