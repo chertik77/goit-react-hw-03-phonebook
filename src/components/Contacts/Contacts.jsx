9037970066a0c08bc74ad3a0813568af3e72b3b3
chertik77 <babychdenya@gmail.com>
2023-11-10 13:52:26 +0000

    updates

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react'
 import { MutatingDots } from 'react-loader-spinner'
 import { useDispatch, useSelector } from 'react-redux'
-import { promiseToast } from '~/notifications/toast'
 import { deleteContactById, fetchContacts } from '~/redux/operations'
 import { filteredContacts } from '~/redux/selectors'
+import { promiseToast } from '../../notifications/message'
 
 export const Contacts = () => {
   const { isLoading } = useSelector(state => state.contacts)
