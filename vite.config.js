121dca28be6e06c390d9cefc1ae9fb50c635bb26
chertik77 <babychdenya@gmail.com>
2023-11-10 11:17:33 +0000

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import FullReload from 'vite-plugin-full-reload'
 
 export default defineConfig({
   plugins: [react(), FullReload(['./**/**.html'])],
-  resolve: { alias: [{ find: '~', replacement: '/src' }] },
+  resolve: {
+    alias: [
+      { find: '~', replacement: '/src' },
+      { find: '~notifications', replacement: '/src/notifications' }
+    ]
+  },
   build: { assetsDir: '' }
 })
