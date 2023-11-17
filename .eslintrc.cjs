d3086210df84cc9fe6840905a2172b3b87ed5425
chertik77 <babychdenya@gmail.com>
2023-11-17 10:38:05 +0000

    move to typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,16 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } },
-  settings: { react: { version: '18.2' } },
+  parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
-  rules: { 'react-refresh/only-export-components': ['warn', { allowConstantExport: true }], 'react/prop-types': 0 },
-  overrides: [{ files: ['*.jsx', '*.js'], parser: '@typescript-eslint/parser' }],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
 }
