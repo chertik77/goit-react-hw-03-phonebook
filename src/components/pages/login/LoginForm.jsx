6cf9af3bbf7a18770d0b56c53eb6e4c795967f29
chertik77 <babychdenya@gmail.com>
2023-11-19 23:39:49 +0000

    make registration

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,10 +1,21 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import { Button, FormControl, FormLabel, Input, Stack } from '@mui/joy'
-import { useLoginFormValidation } from 'hooks/useLoginFormValidation'
+import { useForm } from 'react-hook-form'
 import { useLoginMutation } from 'redux/services'
+import { createValidationSchema } from 'utils/helpers/validationSchema'
 import { RequestError } from 'utils/ui/Error'
+
 export const LoginForm = () => {
   const [login, { isLoading, error }] = useLoginMutation()
-  const { reset, handleSubmit, registerEmail, registerPassword, errorMessage } = useLoginFormValidation()
+  const {
+    reset,
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({ resolver: yupResolver(createValidationSchema(['email', 'password'])) })
+
+  const errorMessage = field =>
+    errors[field] && <small className='p-error mt-2'>{errors[field]?.message}</small>
 
   const onSubmit = data => {
     login(data)
@@ -15,12 +26,12 @@ export const LoginForm = () => {
     <form onSubmit={handleSubmit(onSubmit)} noValidate>
       <FormControl>
         <FormLabel>Email</FormLabel>
-        <Input type='email' name='email' {...registerEmail} sx={{ mb: 1 }} />
+        <Input type='email' name='email' {...register('email')} sx={{ mb: 1 }} />
         {errorMessage('email')}
       </FormControl>
       <FormControl>
         <FormLabel>Password</FormLabel>
-        <Input type='password' name='password' {...registerPassword} sx={{ mb: 1 }} />
+        <Input type='password' name='password' {...register('password')} sx={{ mb: 1 }} />
         {errorMessage('password')}
       </FormControl>
       <Stack sx={{ mt: 0.5, gap: 2 }}>
---
+++ src/utils/helpers/validationSchema.js
@@ -0,0 +1,34 @@
+import { object, string } from 'yup'
+
+const PHONE_NUMBER_LENGTH = 10
+
+const validationSchemas = {
+  name: string()
+    .required('This field is required.')
+    .min(5, 'This field should contain at least 5 characters.')
+    .trim(),
+  number: string()
+    .required('This field is required.')
+    .test(
+      'len',
+      'Please enter a valid phone number with exactly 9 digits.',
+      val => val.replace(/[^0-9]/g, '').length === PHONE_NUMBER_LENGTH
+    ),
+  email: string().email('Please enter a valid email address.').required('This field is required.').trim(),
+  password: string()
+    .required('This field is required.')
+    .min(8, 'This field should contain at least 8 characters.')
+    .trim()
+}
+
+export const createValidationSchema = fields => {
+  let schema = {}
+
+  fields.forEach(field => {
+    if (validationSchemas[field]) {
+      schema[field] = validationSchemas[field]
+    }
+  })
+
+  return object().shape(schema)
+}
