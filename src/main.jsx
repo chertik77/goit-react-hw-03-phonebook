bc3477358950e89703496895a56f43b541465255
chertik77 <babychdenya@gmail.com>
2023-11-20 04:18:53 +0000

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { PrimeReactProvider } from 'primereact/api'
 import 'primereact/resources/themes/lara-light-indigo/theme.css'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { HashRouter as Router } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from 'redux/store'
 import './index.css'
@@ -12,9 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PrimeReactProvider>
       <PersistGate loading={null} persistor={persistor}>
-        <Router basename={import.meta.env.MODE === 'production' ? '/goit-react-hw-08-phonebook' : ''}>
+        <HashRouter basename={import.meta.env.MODE === 'production' ? '/goit-react-hw-08-phonebook' : ''}>
           <App />
-        </Router>
+        </HashRouter>
       </PersistGate>
     </PrimeReactProvider>
   </Provider>
