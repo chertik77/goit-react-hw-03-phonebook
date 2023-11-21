ab549e22cd094cbb51100b45755064510750642b
chertik77 <babychdenya@gmail.com>
2023-11-21 19:45:32 +0000

    updates

--- src/components/pages/contacts/blocks/main/Main.jsx
+++ src/components/pages/contacts/blocks/main/Main.jsx
@@ -1,21 +1,36 @@
-import { Sheet } from '@mui/joy'
+import { Sheet, Typography } from '@mui/joy'
+import { Link } from 'react-router-dom'
 import { Filter } from './Filter'
 import { ContactsTable } from './Table'
 
 export const Main = ({ items, filter }) => (
   <>
     <Filter onChange={filter} />
-    <Sheet
-      variant='outlined'
-      sx={{
-        display: { xs: 'none', sm: 'initial' },
-        width: '100%',
-        borderRadius: 'sm',
-        flexShrink: 1,
-        overflow: 'auto',
-        minHeight: 0
-      }}>
-      <ContactsTable items={items} />
-    </Sheet>
+    {items()?.length > 0 ? (
+      <>
+        <Sheet
+          variant='outlined'
+          sx={{
+            display: { xs: 'none', sm: 'initial' },
+            width: '100%',
+            borderRadius: 'sm',
+            flexShrink: 1,
+            overflow: 'auto',
+            minHeight: 0
+          }}>
+          <ContactsTable items={items} />
+        </Sheet>
+      </>
+    ) : (
+      <>
+        <Typography variant='h3'>You dont have any contacts...</Typography>
+        <Typography variant='h3'>
+          Add someone to start by{' '}
+          <Link to='create' className='text-blue-400 underline'>
+            clicking here
+          </Link>
+        </Typography>
+      </>
+    )}
   </>
 )
