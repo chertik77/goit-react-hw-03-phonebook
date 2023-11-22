b292d088f300f434274cc665d6d91eb473cbf125
chertik77 <babychdenya@gmail.com>
2023-11-22 20:14:30 +0000

    add user refresh

--- src/components/pages/contacts/blocks/main/Main.jsx
+++ src/components/pages/contacts/blocks/main/Main.jsx
@@ -1,5 +1,4 @@
-import { Link, Sheet, Typography } from '@mui/joy'
-import { Link as RouteLink } from 'react-router-dom'
+import { Sheet, Typography } from '@mui/joy'
 import { Filter } from '../Filter'
 import { ContactsTable } from './Table'
 
@@ -13,12 +12,7 @@ export const Main = ({ contacts, filter, setFilter }) => (
         <ContactsTable contacts={contacts} />
       </Sheet>
     ) : filter === '' ? (
-      <Typography variant='h3'>
-        Add someone to start by{' '}
-        <Link to='create' component={RouteLink}>
-          clicking here
-        </Link>
-      </Typography>
+      <Typography variant='h3'>Add someone to start</Typography>
     ) : (
       <Typography variant='h3'>We didnt find any contacts based on your query...</Typography>
     )}
